7b0dbd2a3b04413be36de518a38221b1aee12cb7
denisdiaconu <denisdiaconu117@yahoo.com>
2025-02-25 11:21:01 +0100

    Add navbar functions

--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Nav from './Nav';
 import MobileNav from './MobileNav';
 
 const ResponsiveNav = () => {
-  return <div>
-    <Nav />
-    <MobileNav />
-  </div>;
+  const [showNav, setShowNav] = useState(false);
+  const handleNavShow = () => setShowNav(true);
+  const handleCloseNav = () => setShowNav(false);
+  return (
+    <div>
+      <Nav openNav={handleNavShow}/>
+      <MobileNav showNav={showNav} closeNav={handleCloseNav}/>
+    </div>
+  );
 };
 
 export default ResponsiveNav;
