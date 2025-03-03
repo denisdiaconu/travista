5094c448b7bcb251e158a8d9dd77eb19dd74ae20
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-03 10:45:00 +0100

    Add text to hero

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -12,6 +12,16 @@ const Hero = () => {
         preload="metadata"
         className="w-full h-full object-cover"
       />
+      <div className="absolute w-full h-full z-[100] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <div className="flex flex-col w-full h-full items-center justify-center">
+          <div>
+            <h1 className="text-white text-[25px] text-center md:text-[35px] lg:text-[45px] mb-4 md:mb-0 tracking-[0.7rem] font-bold uppercase">
+              Lets Enjoy The Nature
+            </h1>
+            <p className='text-white md:text-base text-center text-lg font-normal [word-spacing:3px]'>Get the best prices on 2,000,000+ properties, worldwide</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
