93a08a9980c372c24244ce24f1b201cad6105041
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-03 10:32:01 +0100

    Add video to hero background

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
   return (
-    <div className='relative h-full h-[120vh] sm:h-[100vh]'>
-      <div className='bg-gray-800 absolute top-0 left-0 w-full h-full opacity-70'>
-
-      </div>
+    <div className="relative h-full h-[120vh] sm:h-[100vh]">
+      <div className="bg-gray-800 absolute top-0 left-0 w-full h-full opacity-70"></div>
+      <video
+        src="/images/hero1.mp4"
+        muted
+        loop
+        autoPlay
+        preload="metadata"
+        className="w-full h-full object-cover"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
