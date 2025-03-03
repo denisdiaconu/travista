143b11c035a7fd13ecd25716785b8d8fed977b16
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-03 10:27:04 +0100

    Add hero overlay

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='relative h-full h-[120vh] sm:h-[100vh]'>
+      <div className='bg-gray-800 absolute top-0 left-0 w-full h-full opacity-70'>
+
+      </div>
+    </div>
   )
 }
 
