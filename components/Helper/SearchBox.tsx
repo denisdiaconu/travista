e6f2605c743963f0457420172e85845272ecdd67
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-04 11:12:11 +0100

    Add first search input

--- components/Helper/SearchBox.tsx
+++ components/Helper/SearchBox.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { FaMap } from 'react-icons/fa';
 
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  return (
+    <div
+      className="bg-white items-center justify-center rounded-lg p-8 mt-4
+     sm:mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-[95%] sm:w-[80%]"
+    >
+      <div className="flex items-center space-x-6">
+        <FaMap className="w-6 h-6 text-blue-600" />
+        <div>
+          <p className="text-lg font-medium mb-[0.2rem]">Location</p>
+          <input
+            type="text"
+            placeholder="Where are you going?"
+            className="placeholder:text-gray-800 outline-none border-none"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchBox;
