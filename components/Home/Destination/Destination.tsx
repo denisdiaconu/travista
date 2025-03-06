d6b1f05ecea9bfe81930a179f2db53155abc8dec
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-06 11:36:49 +0100

    Add DestinationSlider component

--- components/Home/Destination/Destination.tsx
+++ components/Home/Destination/Destination.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import SectionHeading from '@/components/Helper/SectionHeading';
+import React from 'react';
 
 const Destination = () => {
   return (
-    <div className='pt-20 pb-20'>
-      
-    </div>
-  )
-}
+    <div className="pt-20 pb-20">
+      <SectionHeading heading="Exploring Popular Destination" />
+      <div className='mt-14 mx-auto w-[80%]'>
 
-export default Destination
+      </div>
+    </div>
+  );
+};
+
+export default Destination;
---
+++ components/Home/Destination/DestinationSlider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DestinationSlider = () => {
+  return (
+    <div>DestinationSlider</div>
+  )
+}
+
+export default DestinationSlider
