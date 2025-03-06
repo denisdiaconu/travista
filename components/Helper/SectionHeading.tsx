1925234c806dfc6e5ed7cd27d419c3bbdb99a660
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-06 11:32:27 +0100

    Add style to SectionHeading

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -5,7 +5,14 @@ type Props = {
 };
 
 const SectionHeading = ({ heading }: Props) => {
-  return <div>SectionHeading</div>;
+  return (
+    <div className="w-[80%] mx-auto">
+      <h1 className="text-blue-950 font-bold text-xl sm:text-3xl">{heading}</h1>
+      <p className="text-gray-700 mt-2 sm:text-base text-sm font-medium">
+        Lorem ipsum dolor sit amet consectetur.
+      </p>
+    </div>
+  );
 };
 
 export default SectionHeading;
