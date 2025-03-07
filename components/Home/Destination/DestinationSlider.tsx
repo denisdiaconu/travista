bb3ed97f87be4718abc229f96a8de9337ac60f3c
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-07 11:31:27 +0100

    Add slider overlay

--- components/Home/Destination/DestinationSlider.tsx
+++ components/Home/Destination/DestinationSlider.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+'use client';
+import { destinationData } from '@/data/data';
+import Image from 'next/image';
+import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
@@ -6,24 +9,47 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1324 },
     items: 5,
-    slidesToSlide: 1
+    slidesToSlide: 1,
   },
   tablet: {
     breakpoint: { max: 1324, min: 764 },
     items: 2,
-    slidesToSlide: 1
+    slidesToSlide: 1,
   },
   mobile: {
     breakpoint: { max: 764, min: 0 },
     items: 1,
-    slidesToSlide: 1
-  }
+    slidesToSlide: 1,
+  },
 };
 
 const DestinationSlider = () => {
   return (
-    <div>DestinationSlider</div>
-  )
-}
+    <Carousel
+      responsive={responsive}
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={5000}
+      keyBoardControl={true}
+    >
+      {destinationData.map((data) => {
+        return (
+          <div key={data.id} className="m-3">
+            <div className="relative h-[400px]">
+              <div className="bg-black opacity-25 absolute inset-0 rounded-lg"></div>
+              <Image
+                src={data.image}
+                width={500}
+                height={500}
+                alt={data.country}
+                className="h-full w-full object-cover rounded-lg"
+              />
+            </div>
+          </div>
+        );
+      })}
+    </Carousel>
+  );
+};
 
-export default DestinationSlider
+export default DestinationSlider;
