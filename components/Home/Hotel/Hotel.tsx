b69ee6d79f1039b405956157af679cd53a80749c
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-10 11:11:29 +0100

    Add hotel card component

--- components/Home/Hotel/Hotel.tsx
+++ components/Home/Hotel/Hotel.tsx
@@ -1,12 +1,16 @@
-import SectionHeading from '@/components/Helper/SectionHeading'
-import React from 'react'
+import SectionHeading from '@/components/Helper/SectionHeading';
+import React from 'react';
 
 const Hotel = () => {
   return (
-    <div className='pt-20 pb-20'>
-        <SectionHeading heading='Recommended Hotels'/>
+    <div className="pt-20 pb-20">
+      <SectionHeading heading="Recommended Hotels" />
+      <div
+        className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
+        gap-8 items-center mt-16"
+      ></div>
     </div>
-  )
-}
+  );
+};
 
-export default Hotel
+export default Hotel;
