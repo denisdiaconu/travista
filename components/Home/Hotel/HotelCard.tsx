c2b655ea51fca64c3a98382ea443e5216e1bde6a
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-11 11:33:05 +0100

    Add overlay for hotel card img

--- components/Home/Hotel/HotelCard.tsx
+++ components/Home/Hotel/HotelCard.tsx
@@ -14,13 +14,18 @@ type Props = {
 };
 
 const HotelCard = ({ hotel }: Props) => {
-  return <div>
-    <div className='relative w-full h-[300px] rounded-lg cursor-pointer group overflow-hidden'>
-      <div className='bg-white text-black absolute w-8 h-8 top-4 right-4 z-20 rounded-full flex items-center justify-center flex-col'>
-        <FaHeart className='w-3 h-3'/>
+  return (
+    <div>
+      <div className="relative w-full h-[300px] rounded-lg cursor-pointer group overflow-hidden">
+        <div className="bg-white text-black absolute w-8 h-8 top-4 right-4 z-20 rounded-full flex items-center justify-center flex-col">
+          <FaHeart className="w-3 h-3" />
+        </div>
+        <div className='bg-black absolute inset-0 opacity-20 z-10'>
+
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default HotelCard;
