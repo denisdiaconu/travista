3955e6c3bf17091fe9a165c9669081c72154cc9d
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-11 11:38:09 +0100

    Add img and style to hotel card

--- components/Home/Hotel/HotelCard.tsx
+++ components/Home/Hotel/HotelCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 import { FaHeart } from 'react-icons/fa';
 
@@ -20,9 +21,14 @@ const HotelCard = ({ hotel }: Props) => {
         <div className="bg-white text-black absolute w-8 h-8 top-4 right-4 z-20 rounded-full flex items-center justify-center flex-col">
           <FaHeart className="w-3 h-3" />
         </div>
-        <div className='bg-black absolute inset-0 opacity-20 z-10'>
-
-        </div>
+        <div className="bg-black absolute inset-0 opacity-20 z-10"></div>
+        <Image
+          src={hotel.image}
+          alt={hotel.name}
+          width={500}
+          height={500}
+          className="overflow-hidden h-full w-full transition-all duration-300 object-cover group-hover:scale-110"
+        />
       </div>
     </div>
   );
