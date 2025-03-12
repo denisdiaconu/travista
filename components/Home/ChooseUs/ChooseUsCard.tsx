7c1fd7df4d858f112f2a3b73de36750bfc90b026
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-12 11:17:14 +0100

    Add content to choose us card

--- components/Home/ChooseUs/ChooseUsCard.tsx
+++ components/Home/ChooseUs/ChooseUsCard.tsx
@@ -7,9 +7,23 @@ type Props = {
 };
 
 const ChooseUsCard = ({ image, title }: Props) => {
-  return <div>
-    <Image src={image} width={70} height={70} alt='image' className='mx-auto'/>
-  </div>;
+  return (
+    <div>
+      <Image
+        src={image}
+        width={70}
+        height={70}
+        alt="image"
+        className="mx-auto"
+      />
+      <h1 className="text-gray-900 font-medium mt-6 text-center text-lg">
+        {title}
+      </h1>
+      <p className="text-gray-700 mt-2 text-center text-xs font-medium">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, obcaecati!
+      </p>
+    </div>
+  );
 };
 
 export default ChooseUsCard;
