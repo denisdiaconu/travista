5caa3cb430398b9289581000abcdcfdc4fb7536f
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-12 11:11:04 +0100

    Add choose us card component

--- components/Home/ChooseUs/ChooseUs.tsx
+++ components/Home/ChooseUs/ChooseUs.tsx
@@ -1,7 +1,24 @@
+import SectionHeading from '@/components/Helper/SectionHeading';
 import React from 'react';
+import ChooseUsCard from './ChooseUsCard';
 
 const ChooseUs = () => {
-  return <div>ChooseUs</div>;
+  return (
+    <div className="pt-16 pb-16">
+      <SectionHeading heading="Why Choose Us" />
+      <div className="grid w-[80%] mx-auto mt-20 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 items-center">
+        <div>
+          <ChooseUsCard image="/images/c1.svg" title="Best Price Guarantee" />
+        </div>
+        <div>
+          <ChooseUsCard image="/images/c2.svg" title="Easy & Quick Booking" />
+        </div>
+        <div>
+          <ChooseUsCard image="/images/c3.svg" title="Customer Care 24/7" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChooseUs;
---
+++ components/Home/ChooseUs/ChooseUsCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+  image: string;
+  title: string;
+};
+
+const ChooseUsCard = ({ image, title }: Props) => {
+  return <div>
+    <
+  </div>;
+};
+
+export default ChooseUsCard;
