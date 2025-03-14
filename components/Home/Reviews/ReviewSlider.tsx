6830e9749021bfd1294f271ff87ef585e52a7fdb
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-14 11:41:35 +0100

    Add review text to slider

--- components/Home/Reviews/ReviewSlider.tsx
+++ components/Home/Reviews/ReviewSlider.tsx
@@ -6,9 +6,29 @@ import 'swiper/css';
 import 'swiper/css/effect-cards';
 
 import { EffectCards } from 'swiper/modules';
+import { reviewData } from '@/data/data';
 
 const ReviewSlider = () => {
-  return <div>ReviewSlider</div>;
+  return (
+    <div>
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="md:w-[450px] md:h-[350px] w-[90%] h-[300px]"
+      >
+        {reviewData.map((data) => {
+          return (
+            <SwiperSlide key={data.id} className='bg-white rounded-3xl block'>
+              <div className='w-[80%] mx-auto mt-16'>
+                <p className='text-xs sm:text-sm md:text-base font-semibold'>{data.review}</p>
+              </div>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </div>
+  );
 };
 
 export default ReviewSlider;
