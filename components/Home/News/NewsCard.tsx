359d4c535cafc28a4951c72f9da33bad1418fbee
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-17 12:15:09 +0100

    Add all news card content

--- components/Home/News/NewsCard.tsx
+++ components/Home/News/NewsCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 type Props = {
@@ -7,7 +8,23 @@ type Props = {
 };
 
 const NewsCard = ({ image, date, title }: Props) => {
-  return <div>NewsCard</div>;
+  return (
+    <div>
+      <div className="h-[300px]">
+        <Image
+          src={image}
+          alt={title}
+          width={300}
+          height={300}
+          className="w-full h-full object-cover rounded-lg"
+        />
+      </div>
+      <h1 className="text-gray-950 hover:text-blue-950 transition-all duration-200 cursor-pointer mt-6 text-lg font-semibold">
+        {title}
+      </h1>
+      <p className="text-gray-600 mt-3 text-sm">{date}</p>
+    </div>
+  );
 };
 
 export default NewsCard;
