d1e6e9d3aa2f346f6de0d9e083fb18668c94f2e2
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-17 11:47:48 +0100

    Add news card component

--- components/Home/News/News.tsx
+++ components/Home/News/News.tsx
@@ -2,9 +2,14 @@ import SectionHeading from '@/components/Helper/SectionHeading';
 import React from 'react';
 
 const News = () => {
-  return <div className='pt-16 pb-16'>
-    <SectionHeading heading='Exciting Travel News for You'/>
-  </div>;
+  return (
+    <div className="pt-16 pb-16">
+      <SectionHeading heading="Exciting Travel News for You" />
+      <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 items-center mt-20">
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default News;
