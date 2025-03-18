01f8e84f6245e6cbeff29c6bc86c96114b1c86a0
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-18 11:33:19 +0100

    Add content to newsletter

--- components/Home/Newsletter/Newsletter.tsx
+++ components/Home/Newsletter/Newsletter.tsx
@@ -4,7 +4,8 @@ import { BsEnvelopePaper } from 'react-icons/bs';
 const Newsletter = () => {
   return <div className='bg-black flex items-center justify-center flex-col w-full pt-16 pb-16'>
     <BsEnvelopePaper className='text-white w-16 h-16 mt-20'/>
-    <h1></h1>
+    <h1 className='text-white text-lg sm:text-xl md:text-2xl font-semibold mt-10 tracking-widest'>Your Travel Journey Starts Here</h1>
+    <p className='text-white mt-3 text-xs sm:text-sm'>Sign up to receive the best deals straight to your inbox!</p>
   </div>;
 };
 
