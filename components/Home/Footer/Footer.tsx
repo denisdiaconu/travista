58150515bde77422918234f0e9fa67cbba69c8df
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-19 11:46:49 +0100

    Add company part

--- components/Home/Footer/Footer.tsx
+++ components/Home/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className='pt-16 pb-16'>
+    <div className='w-[80%] mx-auto items-start grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10'>
+        <div className='space-y-5'>
+            <h1 className='text-lg font-bold'>Company</h1>
+            <p className='text-gray-800 text-sm hover:text-blue-950 font-medium cursor-pointer'>About Us</p>
+            <p className='text-gray-800 text-sm hover:text-blue-950 font-medium cursor-pointer'>Careers</p>
+            <p className='text-gray-800 text-sm hover:text-blue-950 font-medium cursor-pointer'>Blogs</p>
+            <p className='text-gray-800 text-sm hover:text-blue-950 font-medium cursor-pointer'>Gift Cards</p>
+            <p className='text-gray-800 text-sm hover:text-blue-950 font-medium cursor-pointer'>Magazine</p>
+        </div>
+    </div>
+  </div>;
 };
 
 export default Footer;
