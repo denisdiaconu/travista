81748da98ad7f4929a40b65e8387da11ce6895ec
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-21 11:27:17 +0100

    Add use effect

--- components/Helper/ScrollToTop.tsx
+++ components/Helper/ScrollToTop.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
 
 const ScrollToTop = () => {
-  return (
-    <div>ScrollToTop</div>
-  )
-}
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener('scroll', toggleVisibility);
+    return window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+  return <div>ScrollToTop</div>;
+};
 
-export default ScrollToTop
+export default ScrollToTop;
