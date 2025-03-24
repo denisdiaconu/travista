a49c1cb963017a70e8d92561c7acc77c0478771c
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-24 11:17:51 +0100

    Add animations setup

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import Hero from './Hero/Hero'
-import Destination from './Destination/Destination'
-import Hotel from './Hotel/Hotel'
-import ChooseUs from './ChooseUs/ChooseUs'
-import Review from './Reviews/Review'
-import News from './News/News'
-import Newsletter from './Newsletter/Newsletter'
+'use client';
+import React, { useEffect } from 'react';
+import Hero from './Hero/Hero';
+import Destination from './Destination/Destination';
+import Hotel from './Hotel/Hotel';
+import ChooseUs from './ChooseUs/ChooseUs';
+import Review from './Reviews/Review';
+import News from './News/News';
+import Newsletter from './Newsletter/Newsletter';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+  useEffect(() => {
+    const initAOS = async () => {
+      await import('aos');
+      AOS.init({
+        duration: 1000,
+        easing: 'ease',
+        once: true,
+        anchorPlacement: 'top-bottom',
+      });
+    };
+    initAOS();
+  }, []);
   return (
-    <div className='overflow-hidden'>
+    <div className="overflow-hidden">
       <Hero />
       <Destination />
       <Hotel />
@@ -18,7 +33,7 @@ const Home = () => {
       <News />
       <Newsletter />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
